ab0998a34d1db135cf21422c66aa16930c73e677
santhiswaroop2016 <31235961+santhiswaroop2016@users.noreply.github.com>
2023-06-20 12:01:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ require __DIR__ . '/vendor/autoload.php';
 use Carbon\Carbon;
 
 
-printf("Right now in New York is %s.\n", Carbon::now('US/Eastern')->format('Y-m-d g:i:s A'));
+printf("Right now in sumeet is %s.\n", Carbon::now('US/Eastern')->format('Y-m-d g:i:s A'));
 
-printf("Tomorrow in New York will be %s.\n", Carbon::tomorrow('US/Eastern')->format('Y-m-d'));
+printf("Tomorrow in balakrishna will be %s.\n", Carbon::tomorrow('US/Eastern')->format('Y-m-d'));
 
-printf("Right now in Paris is %s\n", Carbon::now('Europe/Paris'));
+printf("Right now in uoender is %s\n", Carbon::now('Europe/Paris'));
